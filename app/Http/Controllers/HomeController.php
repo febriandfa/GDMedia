c41bc34491b61a480b639cac02e5e10681f4ec93
okkyarrohman <okkyzee@gmail.com>
2024-02-04 17:01:54 +0700

    sessionLogin and totalLogin users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function guru()
+    {
+        return 'Dashboard Guru';
+    }
+
+    public function murid()
+    {
+        return 'Dashboard Murid';
+    }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $guru = User::create([
+            'name' => 'Guru GDMedia',
+            'email' => 'guru@gdmedia.com',
+            'password' => bcrypt('guru123'),
+            'role' => 'guru'
+        ]);
+        $guru->assignRole('guru');
+
+        $murid = User::create([
+            'name' => 'Murid GDMedia',
+            'email' => 'murid@gdmedia.com',
+            'password' => bcrypt('murid123'),
+            'role' => 'murid'
+        ]);
+        $murid->assignRole('murid');
+    }
+}
