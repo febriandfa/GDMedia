58520b5a1283b90e2aaa28f0c045a04d6d871fd5
okkyarrohman <okkyzee@gmail.com>
2024-02-04 16:08:34 +0700

    role permision from spatie

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Landgin Pages
+Route::view('/', 'landingPages');
+
+// Route Admin
+
+
+
+// Route Siswa
