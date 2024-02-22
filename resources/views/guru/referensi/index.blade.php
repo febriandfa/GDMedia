385c396558bcb5df8bacb92f43f51872e026b831
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-02-22 10:02:27 +0700

    referensi index

--- resources/views/guru/referensi/index.blade.php
+++ resources/views/guru/referensi/index.blade.php
@@ -1,1 +1,55 @@
 @extends('layouts.guruLayout')
+
+@section('content')
+<div class="flex justify-between items-center pb-6 mb-6 border-b-2 border-b-abu-400">
+    <x-title title="Referensi" />
+    <div class="flex items-center gap-4">
+        <x-siswa.search-bar />
+        <button data-modal-target="default-modal" data-modal-toggle="default-modal" type="button" class="text-[1.75rem] text-white size-14 rounded-full bg-hijau-400">+</button>
+    </div>
+</div>
+
+<!-- Main modal -->
+<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 rounded-t">
+                <h3 class="text-xl font-semibold">
+                    Tambahkan Referensi Desain
+                </h3>
+                <button type="button" class="bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form method="POST" action="{{ route('referensi-guru.store') }}">
+            @csrf
+                <div class="p-4 md:p-5 space-y-9">
+                    <div class="flex flex-col gap-8">
+                        <input type="text" id="sumber" name="sumber" value="{{ old('sumber') }}" placeholder="Masukkan Sumber Referensi" required autofocus class="w-full px-4 py-2 border-2 outline-none border-hijau-400 rounded-xl focus:border-hijau focus:border-2">
+                    </div>
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-4 md:p-5 rounded-b gap-4 justify-end w-full">
+                    <button data-modal-hide="default-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100">Tutup</button>
+                    <button data-modal-hide="default-modal" type="submit" class="text-white bg-hijau hover:bg-hijau-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+{{-- Masonry Gallery --}}
+<div class="columns-4 gap-6">
+    @foreach ($referensis as $referensi)
+    <div class="h-auto max-w-60 mb-6">
+        <img src="https://source.unsplash.com/random/1" alt="Referensi Image {{ $referensi->id }}" class="rounded-xl">
+    </div>
+    @endforeach
+</div>
+@endsection
---
+++ resources/views/murid/referensi/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.siswaLayout')
+
+@section('content')
+<div class="flex justify-between items-center pb-6 mb-6 border-b-2 border-b-abu-400">
+    <x-title title="Referensi" />
+    <x-siswa.search-bar />
+</div>
+
+{{-- Masonry Gallery --}}
+<div class="columns-4 gap-6">
+    @foreach ($referensis as $referensi)
+    <div class="h-auto max-w-60 mb-6">
+        <img src="https://source.unsplash.com/random/1" alt="Referensi Image {{ $referensi->id }}" class="rounded-xl">
+    </div>
+    @endforeach
+</div>
+@endsection
