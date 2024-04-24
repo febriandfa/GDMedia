673833b4af4f8ad1c537b048ba777b58575fc56e
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-04-25 05:36:28 +0700

    feat: add instrumen penilaian for guru

--- resources/views/guru/progress/show.blade.php
+++ resources/views/guru/progress/show.blade.php
@@ -39,11 +39,18 @@
     <div class="w-full rounded-xl border-b border-b-hijau py-4 px-8 bg-hijau-200 mt-16">
         <p class="text-xl font-semibold text-center">Nilai : {{ $tugasNilai->nilai }}</p>
     </div>
-@else
-    <div class="w-full">
+{{-- @else --}}
+    {{-- <div class="w-full">
+        <button class="block py-2.5 px-5 ms-3 text-sm font-medium focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100">Lihat Instrumen Penilaian</button>
         <button data-modal-target="default-modal" data-modal-toggle="default-modal" type="button" class="p-4 rounded-xl bg-hijau text-lg text-white font-semibold mt-16 block w-fit mx-auto">Tambahkan Nilai</button>
-    </div>
+    </div> --}}
 @endif
+<div class="w-full flex items-center justify-end gap-6">
+    <button data-modal-target="default-modal-pdf" data-modal-toggle="default-modal-pdf" type="button" class="p-4 rounded-xl bg-white border-2 border-hijau text-lg text-hijau font-semibold mt-16 block w-fit">Lihat Instrumen Penilaian</button>
+    @if (!$tugasNilai)
+        <button data-modal-target="default-modal" data-modal-toggle="default-modal" type="button" class="p-4 rounded-xl bg-hijau text-lg text-white font-semibold mt-16 block w-fit">Tambahkan Nilai</button>
+    @endif
+</div>
 
 <!-- Main modal -->
 <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -79,6 +86,38 @@
                     <button data-modal-hide="default-modal" type="submit" class="text-white bg-hijau hover:bg-hijau-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Simpan</button>
                 </div>
             </form>
+        </div>
+    </div>
+</div>
+
+<div id="default-modal-pdf" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 rounded-t">
+                <h3 class="text-xl font-semibold">
+                    Instrumen Penilaian
+                </h3>
+                <button type="button" class="bg-transparent rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal-pdf">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div>
+                <div class="p-4 md:p-5 space-y-9">
+                    <embed class="block rounded-xl w-full h-screen bg-[#D9D9D9]"
+                    src="{{ asset('assets/instrumen-penilaian.pdf')}}" type="application/pdf">
+                    </embed>
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center p-4 md:p-5 rounded-b gap-4 justify-end w-full">
+                    <button data-modal-hide="default-modal-pdf" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100">Tutup</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
