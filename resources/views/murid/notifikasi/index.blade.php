d045284232e4b61654cf0daf45664265dd0c2502
okkyarrohman <okkyzee@gmail.com>
2024-05-01 12:24:30 +0700

    push

--- resources/views/murid/notifikasi/index.blade.php
+++ resources/views/murid/notifikasi/index.blade.php
@@ -8,41 +8,33 @@
             <p class="text-lg font-semibold">{{ auth()->user()->name }}</p>
         </div>
     </div>
+    @php
+        $notifikasiId = $notifikasi->id;
+        $userId = auth()->user()->id;
+
+        $notifikasiFilter = $notifikasi->notifikasi_seens->filter(function ($notifikasi) use ($notifikasiId, $userId) {
+            return $notifikasi->notifikasi_id == $notifikasiId && $notifikasi->user_id == $userId;
+        });
+    @endphp
+
+    @php
+        $notifikasiFilter = $notifikasi->notifikasi_seens->filter(function ($notifikasi) use ($notifikasiId, $userId) {
+            return $notifikasi->notifikasi_id == $notifikasiId && $notifikasi->user_id == $userId;
+        });
+    @endphp
+
+
+    <form action="{{ route('notifikasi.lihatSemua') }}" method="POST">
+        @csrf
+        <button type="submit" {{ count($notifikasiFilter) != 0 ? 'disabled' : '' }}
+            class="text-lg font-semibold m-4 px-4 py-2 rounded-xl bg-white border border-hijau text-hijau">Tandai
+            Dibaca Semua</button>
+    </form>
 
     <div class="space-y-6">
         @foreach ($notifikasis as $notifikasi)
-            @php
-                $notifikasiId = $notifikasi->id;
-                $userId = auth()->user()->id;
-
-                $notifikasiFilter = $notifikasi->notifikasi_seens->filter(function ($notifikasi) use (
-                    $notifikasiId,
-                    $userId,
-                ) {
-                    return $notifikasi->notifikasi_id == $notifikasiId && $notifikasi->user_id == $userId;
-                });
-            @endphp
-
-            @php
-                $notifikasiFilter = $notifikasi->notifikasi_seens->filter(function ($notifikasi) use (
-                    $notifikasiId,
-                    $userId,
-                ) {
-                    return $notifikasi->notifikasi_id == $notifikasiId && $notifikasi->user_id == $userId;
-                });
-            @endphp
-
-            <form action="{{ route('notifikasi.lihatSemua') }}" method="POST">
-                @csrf
-                <button type="submit" {{ count($notifikasiFilter) != 0 ? 'disabled' : '' }}
-                    class="text-lg font-semibold m-4 px-4 py-2 rounded-xl bg-white border border-hijau text-hijau">Tandai
-                    Dibaca Semua</button>
-            </form>
-
-
             <form method="POST" action="{{ route('notifikasi.markSeen') }}">
                 @csrf
-
 
                 <input type="text" id="notifikasi_id" name="notifikasi_id" value="{{ $notifikasi->id }}" class="hidden">
                 <button type="submit" {{ count($notifikasiFilter) != 0 ? 'disabled' : '' }}
