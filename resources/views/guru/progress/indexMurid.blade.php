7c860a4c668aebee1604fb2b8041735a2223be0c
okkyarrohman <okkyzee@gmail.com>
2024-05-26 12:36:34 +0700

    cos

--- resources/views/guru/progress/indexMurid.blade.php
+++ resources/views/guru/progress/indexMurid.blade.php
@@ -14,75 +14,53 @@
     </a>
 
     <div class="space-y-6">
-
-        @foreach ($tugases->subtugas[0]->tugas_answer as $tugas_answer)
+        @foreach ($users as $user)
             @php
-                $userFilter = $users->where('id', $tugas_answer->user_id);
-
-                $sortedFilter = $users->orderBy('name', 'ASC');
-
+                $userAnswers = $user->tugas_answer->where('subtugas.tugas.id', $tugases->id);
+                $subtugasLength = $subtugases->count();
+                $answerLength = $userAnswers->count();
+                $answerPercentage = $subtugasLength > 0 ? ($answerLength / $subtugasLength) * 100 : 0;
+                $userGrade = $tugases->tugas_nilai->where('murid_id', $user->id)->first();
             @endphp
 
-            @foreach ($sortedFilter as $user)
-                @php
-                    // $answerFilter = $answers->where('user_id', $user->id);
-
-                    $userId = $user->id;
-                    $tugasId = $tugases->id;
-
-                    $answerFilter = $answers->filter(function ($answer) use ($tugasId, $userId) {
-                        return $answer->subtugas->tugas->id == $tugasId && $answer->user_id == $userId;
-                    });
-
-                    $subtugasLength = $subtugases->count();
-
-                    $answerLength = $answerFilter->count();
-
-                    $answerPercentage = ($answerLength / $subtugasLength) * 100;
-
-                    $userGrade = $tugases->tugas_nilai->where('murid_id', $userId)->first();
-                @endphp
-
-                <div class="bg-white rounded-xl border-b border-b-hijau p-6 flex items-center justify-between">
-                    <div class="flex items-center gap-4">
-                        @if ($user->foto)
-                            <img class="size-16 rounded-full" src="{{ asset('storage/profile/foto/' . $user->foto) }}"
-                                alt="Icon User">
-                        @else
-                            <img class="size-16 rounded-full" src="{{ asset('assets/profile-icon.png') }}" alt="Icon User">
-                        @endif
-                        <p class="text-lg font-semibold">{{ $user->name }}</p>
-                    </div>
-                    <div class="flex items-center gap-4">
-                        <div class="flex items-center justify-center w-fit" x-data="{ circumference: 2 * 22 / 7 * 23 }">
-                            <svg class="w-16 h-16 transform -rotate-90">
-                                <circle cx="32.5" cy="32.5" r="23" stroke="currentColor" stroke-width="8"
-                                    fill="transparent" class="text-abu-400" />
-                                <circle cx="32.5" cy="32.5" r="23" stroke="currentColor" stroke-width="8"
-                                    fill="transparent" :stroke-dasharray="circumference"
-                                    :stroke-dashoffset="circumference - {{ $answerPercentage }} / 100 * circumference"
-                                    class="text-hijau" />
-                            </svg>
-                            <span
-                                class="absolute text-xs">{{ round($answerPercentage, 2) ? round($answerPercentage, 1) : 0 }}%</span>
-                        </div>
-                        @if ($userGrade)
-                            <div
-                                class="py-1.5 px-8 rounded-xl bg-white border-2 border-hijau text-lg text-hijau font-semibold block w-fit">
-                                Nilai : {{ $userGrade->nilai }}</div>
-                        @endif
-                        <a href="{{ route('progress-guru.show', $user->id) }}"
-                            class="py-2 px-8 rounded-xl bg-hijau text-white text-lg font-semibold">
-                            Lihat
-                        </a>
-                    </div>
+            <div class="bg-white rounded-xl border-b border-b-hijau p-6 flex items-center justify-between">
+                <div class="flex items-center gap-4">
+                    @if ($user->foto)
+                        <img class="size-16 rounded-full" src="{{ asset('storage/profile/foto/' . $user->foto) }}"
+                            alt="Icon User">
+                    @else
+                        <img class="size-16 rounded-full" src="{{ asset('assets/profile-icon.png') }}" alt="Icon User">
+                    @endif
+                    <p class="text-lg font-semibold">{{ $user->name }}</p>
                 </div>
-            @endforeach
+                <div class="flex items-center gap-4">
+                    <div class="flex items-center justify-center w-fit" x-data="{ circumference: 2 * 22 / 7 * 23 }">
+                        <svg class="w-16 h-16 transform -rotate-90">
+                            <circle cx="32.5" cy="32.5" r="23" stroke="currentColor" stroke-width="8"
+                                fill="transparent" class="text-abu-400" />
+                            <circle cx="32.5" cy="32.5" r="23" stroke="currentColor" stroke-width="8"
+                                fill="transparent" :stroke-dasharray="circumference"
+                                :stroke-dashoffset="circumference - {{ $answerPercentage }} / 100 * circumference"
+                                class="text-hijau" />
+                        </svg>
+                        <span
+                            class="absolute text-xs">{{ round($answerPercentage, 2) ? round($answerPercentage, 1) : 0 }}%</span>
+                    </div>
+                    @if ($userGrade)
+                        <div
+                            class="py-1.5 px-8 rounded-xl bg-white border-2 border-hijau text-lg text-hijau font-semibold block w-fit">
+                            Nilai : {{ $userGrade->nilai }}</div>
+                    @endif
+                    <a href="{{ route('progress-guru.show', $user->id) }}"
+                        class="py-2 px-8 rounded-xl bg-hijau text-white text-lg font-semibold">
+                        Lihat
+                    </a>
+                </div>
+            </div>
         @endforeach
     </div>
 
     <script>
         console.log(@json($tugases))
-        console.log(@json($answerFilter))
     </script>
 @endsection
